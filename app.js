18d342db8d48d8c0de67073ed24641d67d3a9be7
Ondrejoda <ondrejoda@gmail.com>
2024-01-21 00:39:44 +0100

    tetetettett

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 let week = [
     [ // monday 
-        {name: "OPH", start: "8:00"}, {name: "pause", start: "8:45"}, {name: "CIT", start: "8:55"}, {name: "pause", start: "9:40"}, {name: "CJL", start: "10:00"}, {name: "free", start: "10:45"}, {name: "ANJ", start: "11:50"}, {name: "pause", start: "12:35"}, {name: "PRG", start: "12:45"}, {name: "pause", start: "13:30"}, {name: "PRA", start: "13:40"}, {name: "end", start: "15:40"}
+        {name: "OPH", start: "8:00"}, {name: "pauza", start: "8:45"}, {name: "CIT", start: "8:55"}, {name: "pauza", start: "9:40"}, {name: "CJL", start: "10:00"}, {name: "volno", start: "10:45"}, {name: "ANJ", start: "11:50"}, {name: "pauza", start: "12:35"}, {name: "PRG", start: "12:45"}, {name: "pauza", start: "13:30"}, {name: "PRA", start: "13:40"}, {name: "end", start: "15:40"}
     ],
     [ // tuesday
-        {name: "MAT", start: "8:00"}, {name: "pause", start: "8:45"}, {name: "FYZ", start: "8:55"}, {name: "pause", start: "9:40"}, {name: "ZSV", start: "10:00"}, {name: "free", start: "10:45"}, {name: "OPH", start: "11:50"}, {name: "pause", start: "13:20"}, {name: "FYZ", start: "13:40"}, {name: "end", start: "14:25"}
+        {name: "MAT", start: "8:00"}, {name: "pauza", start: "8:45"}, {name: "FYZ", start: "8:55"}, {name: "pauza", start: "9:40"}, {name: "ZSV", start: "10:00"}, {name: "volno", start: "10:45"}, {name: "OPH", start: "11:50"}, {name: "pauza", start: "13:20"}, {name: "FYZ", start: "13:40"}, {name: "end", start: "14:25"}
     ],
     [ // wednesday
-        {name: "DBS", start: "8:00"}, {name: "pause", start: "8:45"}, {name: "CIT", start: "8:55"}, {name: "pause", start: "9:40"}, {name: "ANJ", start: "10:00"}, {name: "free", start: "10:45"}, {name: "OPH", start: "11:50"}, {name: "pause", start: "12:35"}, {name: "ZSV", start: "12:45"}, {name: "pause", start: "13:30"}, {name: "MAT", start: "13:40"}, {name: "pause", start: "14:25"}, {name: "DBS", start: "14:35"}, {name: "end", start: "16:05"}
+        {name: "DBS", start: "8:00"}, {name: "pauza", start: "8:45"}, {name: "CIT", start: "8:55"}, {name: "pauza", start: "9:40"}, {name: "ANJ", start: "10:00"}, {name: "volno", start: "10:45"}, {name: "OPH", start: "11:50"}, {name: "pauza", start: "12:35"}, {name: "ZSV", start: "12:45"}, {name: "pauza", start: "13:30"}, {name: "MAT", start: "13:40"}, {name: "pauza", start: "14:25"}, {name: "DBS", start: "14:35"}, {name: "end", start: "16:05"}
     ],
     [ // thursday
-        {name: "CJL", start: "8:00"}, {name: "pause", start: "8:45"}, {name: "WEB", start: "8:55"}, {name: "pause", start: "9:40"}, {name: "MAT", start: "10:00"}, {name: "pause", start: "10:45"}, {name: "ANJ", start: "10:55"}, {name: "pause", start: "11:40"}, {name: "TEV", start: "11:50"}, {name: "end", start: "13:20"}
+        {name: "CJL", start: "8:00"}, {name: "pauza", start: "8:45"}, {name: "WEB", start: "8:55"}, {name: "pauza", start: "9:40"}, {name: "MAT", start: "10:00"}, {name: "pauza", start: "10:45"}, {name: "ANJ", start: "10:55"}, {name: "pauza", start: "11:40"}, {name: "TEV", start: "11:50"}, {name: "end", start: "13:20"}
     ],
     [ // friday
-        {name: "MME", start: "8:00"}, {name: "pause", start: "9:30"}, {name: "PRG", start: "10:00"}, {name: "pause", start: "11:30"}, {name: "WEB", start: "11:50"}, {name: "end", start: "13:20"}
+        {name: "MME", start: "8:00"}, {name: "pauza", start: "9:30"}, {name: "PRG", start: "10:00"}, {name: "pauza", start: "11:30"}, {name: "WEB", start: "11:50"}, {name: "end", start: "13:20"}
     ],
     [], [] // saturday, sunday
 ];
 
 let trams = [
-    "3 13:08", "2 13:16", "3 13:23", "2 13:31", "3 13:38", "2 13:46", "3 13:53", 
+    "3 13:08", "2 13:16", "3 13:23", "2 13:31", "3 13:38", "2 13:46", "3 13:53",
     "2 14:01", "3 14:08", "2 14:16", "3 14:23", "2 14:31", "3 14:38", "2 14:46", "3 14:53",
     "2 15:01", "3 15:08", "2 15:16", "3 15:23", "2 15:31", "3 15:38", "2 15:46", "3 15:53",
     "2 16:01", "3 16:08", "2 16:16", "3 16:23", "2 16:31", "3 16:38", "2 16:46", "3 16:53",
@@ -57,35 +57,21 @@ function addElement(elementTexts, elementClass, targetID) {
 function calcEtaTime(currTime, nextTime) {
     let etaTime = nextTime - currTime;
     if (etaTime == 0) {
-        return "now";
+        return "nyní";
     }
     let etaMinutes = etaTime % 60;
     let etaHours = (etaTime - etaMinutes) / 60;
-    let etaText = "in ";
+    let etaText = "za ";
     if (etaHours > 0) {
-        if (etaHours > 1) {
-            etaText += etaHours + " hrs ";
-        } else {
-            etaText += "1 hr ";
-        }
+        etaText += etaHours + " hod";
     }
-    if (etaMinutes > 1) {
-        etaText += etaMinutes + " mins";
-    } else {
-        etaText += "1 min";
+    if (etaMinutes > 0) {
+        etaText += " " + etaMinutes + " min";
     }
     return etaText;
 }
 
-function max(x, y) {
-    if (y > x) {
-        return y;
-    } else {
-        return x;
-    }
-}
-
-let testing = true;
+let testing = false;
 
 window.addEventListener("load", (event) => {
     const date = new Date();
@@ -128,8 +114,10 @@ window.addEventListener("load", (event) => {
             texts.push(hour.name);
             texts.push(timeArr[0] + ":" + timeArr[1] + " - " + futureTimeArr[0] + ":" + futureTimeArr[1] + " (" + etaText + ")");
 
-            if (hour.name == "pause" || hour.name == "free") {
+            if (hour.name == "pauza") {
                 addElement(texts, "schedule__item schedule__item--pause", "today");
+            } else if (hour.name == "volno") {
+                addElement(texts, "schedule__item schedule__item--free", "today");
             } else {
                 addElement(texts, "schedule__item", "today");
             }
